a413116c56fc680d5f3183cdd1716c56a87bcc99
Pratyay Rudra <rudrapratyay1999@gmail.com>
2020-06-04 18:31:15 +0530

    db

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
+//Important
 document.querySelector(".project-full").style.display = "none"
 document.querySelector(".close").style.display = "none"
+let db;
+let projects;
+let educations;
+let experiences;
+let achievements;
+
+initialize = async () => {
+    db = await (await fetch('/assets/data/db.json')).json();
+    projects = db.projects;
+    educations = db.educations;
+    experiences = db.experiences;
+    achievements = db.achievements;
+    setProjects();
+    setEducations();
+    setExperiences();
+    setAchievements();
+}
 
 displayProjects = () => {
     document.querySelector(".content").style.display = "none"
@@ -18,3 +36,105 @@ closeProjects = () => {
     }
     document.querySelector(".content").style.display = "block"
 }
+
+setProjects = async () => {
+
+    let fewProjects = "";
+    projects.slice(0, 2).forEach(project => {
+        let html = `
+            <div class="project-item">
+                <img src="${project.image}" alt="">
+                <div class="project-item-content">
+                    <h4>${project.title}</h4>
+                    <h5>${project.subTitle}</h5>
+                    <p>${project.description}</p>
+                    <div class="project-item-footer">
+                        <span>
+        `;
+        project.technologies.forEach(technology => {
+            let tech = `
+                <img src="assets/icons/icons8-${technology}-50.png" alt="${technology}">
+            `;
+            html += tech;
+        })
+        html += `
+                    </span>
+                    <a class="btn">View Project</a>
+                </div>
+
+            </div>
+            </div>
+        `
+        fewProjects += html;
+    })
+    document.querySelector("#projectContent").innerHTML = fewProjects;
+
+}
+
+setEducations = async () => {
+
+    let educationContent = "";
+    educations.forEach(education => {
+        let html = `
+            <div class="education-item">
+                <div class="details">
+                    <h4>${education.university}</h4>
+                    <h5>${education.degree}</h5>
+                    <p>${education.institute}</p>
+                    <p>${education.grade ? education.grade : ""}</p>
+                </div>
+                <div class="timeline">
+                    ${education.timeline}
+                </div>
+            </div>
+        `;
+        educationContent += html;
+    });
+    document.querySelector("#educationContent").innerHTML = educationContent;
+
+}
+
+setExperiences = async () => {
+
+    let experienceContent = "";
+    experiences.forEach(experience => {
+        let html = `
+            <div class="experience-item">
+                <div class="details">
+                    <h4>${experience.role}</h4>
+                    <h5>${experience.company}</h5>
+                    <p>${experience.description}</p>
+                </div>
+                <div class="timeline">
+                    ${experience.timeline}
+                </div>
+            </div>
+        `
+        experienceContent += html;
+    })
+    document.querySelector("#experienceContent").innerHTML = experienceContent;
+
+}
+
+setAchievements = async () => {
+
+    let achievementContent = "";
+    achievements.forEach(achievement => {
+        let html = `
+            <div class="achievement-item">
+                <div class="details">
+                    <h4>${achievement.name}</h4>
+                    <p>${achievement.description}</p>
+                </div>
+                <div class="timeline">
+                    <p>${achievement.timeline}</p>
+                    <a class="btn" href="${achievement.link}">View</a>
+                </div>
+            </div>
+        `
+        achievementContent += html;
+    })
+    document.querySelector('#achievementContent').innerHTML = achievementContent;
+
+}
+
